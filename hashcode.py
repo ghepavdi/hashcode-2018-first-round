5177d48fc18c832f2898b3051d7835e00134d691
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2018-03-01 19:54:45 +0100

    File fix

--- hashcode.py
+++ hashcode.py
@@ -6,7 +6,12 @@ Hashcode 2018 first round
 
 import sys
 import numpy as np
+from enum import Enum
 
+class VehicleState(Enum):
+    NO_RIDE = 0
+    GOING_TO_RIDE = 1
+    ON_RIDE = 2
 
 def get_ride_start(ride):
     return [ride[0], ride[1]]
@@ -17,23 +22,38 @@ def get_ride_end(ride):
 def get_ride_times(ride):
     return [ride[4], ride[5]]
 
+def get_ride_time_to_complete(ride):
+    return distance(get_ride_start(ride), get_ride_end(ride))
+
+def get_vehicle_state(vehicle):
+    return vehicle[0]
+
+def get_vehicle_position(vehicle):
+    return [vehicle[1], vehicle[2]]
+
+def get_vehicle_ride(vehicle):
+    state = get_vehicle_state(vehicle)
+    if state == VehicleState.GOING_TO_RIDE or state == VehicleState.ON_RIDE:
+        return vehicle[3]
+
 def distance(start_point, end_point):
     a, b = start_point
     x, y = end_point
     return abs(a - x) + abs(b - y) 
 
 
-def calculate_ride_score(rides,bonus,ride_id,current_step):
+def calculate_ride_score(rides,bonus,ride_id,started_time,finished_time):
     score=0
     current_ride=rides[ride_id]#xstart, ystart, xfinish, yfinish, earliest_start, latest_finish
     x_start,y_start=get_ride_start(current_ride)
     x_finish, y_finish=get_ride_end(current_ride)
     earliest_start, latest_finish=get_ride_times(current_ride)
-    """if finished_time> latest_finish:
-        return 0# I finished too late, no score"""
-    if current_step==earliest_start:
+    if finished_time> latest_finish:
+        return 0# I finished too late, no score
+    if started_time==earliest_start:
         score+=bonus#yay I started in time
-    score+=distance((x_start,y_start),(x_finish,y_finish))
+    number_of_steps=abs(x_start-x_finish)+abs(y_start-y_finish)
+    score+=number_of_steps
     return score
 
 
@@ -76,11 +96,21 @@ def write_output(filename, things_to_score):
         # for s in things_to_score:
         #     file.write("{} {} {} {}\n".format(s[0], s[1], s[2], s[3]))
 
-
 def calculate_score(score_input):
     score = 0
     return score
 
+def ride_can_be_made(ride, current_step, max_step):
+    time_to_complete_ride = get_ride_time_to_complete(ride)
+    # NOTE: <= ???
+    return (current_step + time_to_complete_ride) <= max_step
+
+def ride_can_be_made_by_vehicle(vehicle, ride, current_step, max_step):
+    vehicle_pos = get_vehicle_position(vehicle)
+    ride_start_pos = get_ride_start(ride)
+    time_to_get_to_ride = distance(vehicle_pos, ride_start_pos)
+    time_to_complete_ride = get_ride_time_to_complete(ride)
+    return (current_step + time_to_get_to_ride + time_to_complete_ride) <= max_step
 
 
 def create_vehicles(n_vehicles):

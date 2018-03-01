b32c7293be1a997a4ed50f16e59646c358eb7167
Stefano Romanazzi <stefan.romanazzi@gmail.com>
2018-03-01 20:49:56 +0100

    Output works

---
+++ additional.py
@@ -0,0 +1,17 @@
+
+
+def ride_is_optimal(vehicle, ride):
+    if vehicle[4] == ride[4]:
+        return True
+
+def write_output(vehicles):
+    submission = open('output.txt', 'w')
+    for vehicle in vehicles:
+        dim = len(vehicle[3])
+        submission.write("%d " % dim)
+        for i in range(dim):
+            submission.write("%d " % vehicle[3][i])
+        submission.write("\n")
+
+vehicles = [0, 0, 0, [1, 2, 3]], [0, 0, 0, [4, 5]]
+write_output(vehicles)
